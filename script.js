0bc9da8696c87deb4034e35e46bbcfed6f7e16dd
xAzeem <154564428+xAzeem@users.noreply.github.com>
2023-12-22 07:23:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        450: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        991: {
+          slidesPerView: 4,
+        },
+        1200: {
+          slidesPerView: 5,
+        },
+      },
+});
